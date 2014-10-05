a25acb27c2221129e41d556b9d4bee3c3e9b69a6
James Reeves <jreeves@weavejester.com>
2014-10-05 20:59:24 +0100

    Add description and URL to project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject meta-merge "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A standalone implementation of Leiningen's meta-merge function"
+  :url "https://github.com/weavejester/meta-merge"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
