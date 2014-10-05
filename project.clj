e875e813cb17a4002e8cda1165f5dd63fb1b4407
James Reeves <jreeves@weavejester.com>
2014-10-05 21:35:45 +0100

    Add support for both Clojure 1.5.1 and 1.6.0

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/weavejester/meta-merge"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :aliases {"test-all" ["with-profile" "default:+1.6" "test"]}
+  :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}})
