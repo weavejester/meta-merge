70ed3c282aa9262801fd725b2794163f36e189aa
James Reeves <jreeves@weavejester.com>
2014-10-05 17:20:32 +0100

    Copy meta-merge function from Leiningen

--- src/meta_merge/core.clj
+++ src/meta_merge/core.clj
@@ -1,6 +1,103 @@
-(ns meta-merge.core)
+(ns meta-merge.core
+  (:require [clojure.set :as set]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn- meta*
+  "Returns the metadata of an object, or nil if the object cannot hold
+  metadata."
+  [obj]
+  (if (instance? clojure.lang.IObj obj)
+    (meta obj)
+    nil))
+
+(defn- with-meta*
+  "Returns an object of the same type and value as obj, with map m as its
+  metadata if the object can hold metadata."
+  [obj m]
+  (if (instance? clojure.lang.IObj obj)
+    (with-meta obj m)
+    obj))
+
+(defn- displace?
+  "Returns true if the object is marked as displaceable"
+  [obj]
+  (-> obj meta* :displace))
+
+(defn- replace?
+  "Returns true if the object is marked as replaceable"
+  [obj]
+  (-> obj meta* :replace))
+
+(defn- top-displace?
+  "Returns true if the object is marked as top-displaceable"
+  [obj]
+  (-> obj meta* :top-displace))
+
+(defn- different-priority?
+  "Returns true if either left has a higher priority than right or vice versa."
+  [left right]
+  (boolean
+   (or (some (some-fn nil? displace? replace?) [left right])
+       (top-displace? left))))
+
+(defn- remove-top-displace [obj]
+  (if-not (top-displace? obj)
+    obj
+    (vary-meta obj dissoc :top-displace)))
+
+(defn- pick-prioritized
+  "Picks the highest prioritized element of left and right and merge their
+  metadata."
+  [left right]
+  (cond (nil? left) right
+        (nil? right) (remove-top-displace left)
+
+        (top-displace? left) right
+
+        (and (displace? left)   ;; Pick the rightmost
+             (displace? right)) ;; if both are marked as displaceable
+        (with-meta* right
+          (merge (meta* left) (meta* right)))
+
+        (and (replace? left)    ;; Pick the rightmost
+             (replace? right))  ;; if both are marked as replaceable
+        (with-meta* right
+          (merge (meta* left) (meta* right)))
+
+        (or (displace? left)
+            (replace? right))
+        (with-meta* right
+          (merge (-> left meta* (dissoc :displace))
+                 (-> right meta* (dissoc :replace))))
+
+        (or (replace? left)
+            (displace? right))
+        (with-meta* left
+          (merge (-> right meta* (dissoc :displace))
+                 (-> left meta* (dissoc :replace))))))
+
+(defn meta-merge
+  "Recursively merge values based on the information in their metadata."
+  [left right]
+  (cond (different-priority? left right)
+        (pick-prioritized left right)
+
+        (-> left meta :reduce)
+        (-> left meta :reduce
+            (reduce left right)
+            (with-meta (meta left)))
+
+        (and (map? left) (map? right))
+        (merge-with meta-merge left right)
+
+        (and (set? left) (set? right))
+        (set/union right left)
+
+        (and (coll? left) (coll? right))
+        (if (or (-> left meta :prepend)
+                (-> right meta :prepend))
+          (-> (concat right left)
+              (with-meta (merge (meta left)
+                                (select-keys (meta right) [:displace]))))
+          (concat left right))
+        
+        :else right))
--- test/meta_merge/core_test.clj
+++ test/meta_merge/core_test.clj
@@ -2,6 +2,35 @@
   (:require [clojure.test :refer :all]
             [meta-merge.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-meta-merge
+  (testing "simple merge"
+    (is (= (meta-merge {:a 1 :b 2} {:b 3 :c 4})
+           {:a 1 :b 3 :c 4})))
+
+  (testing "inner map merge"
+    (is (= (meta-merge {:a {:b 1 :c 2}} {:a {:c 3}})
+           {:a {:b 1 :c 3}})))
+
+  (testing "inner set merge"
+    (is (= (meta-merge {:a #{:b :c}} {:a #{:c :d}})
+           {:a #{:b :c :d}})))
+
+  (testing "inner vector merge"
+    (is (= (meta-merge {:a [:b :c]} {:a [:d]})
+           {:a [:b :c :d]})))
+
+  (testing "meta replace"
+    (is (= (meta-merge {:a [:b :c]} {:a ^:replace [:d]})
+           {:a [:d]})))
+
+  (testing "meta displace"
+    (is (= (meta-merge {:a [:b :c]} {:a ^:displace [:d]})
+           {:a [:b :c]})))
+
+  (testing "meta prepend"
+    (is (= (meta-merge {:a [:b :c]} {:a ^:prepend [:d]})
+           {:a [:d :b :c]})))
+
+  (testing "deep inner merge"
+    (is (= (meta-merge {:a {:b {:c [:d]}}} {:a {:b {:c [:e] :f :g}}})
+           {:a {:b {:c [:d :e] :f :g}}}))))
