c8890bcf9af4e07de7f52e800e84dc279b37b956
Dominic Monroe <iam@dominic.io>
2015-12-13 11:44:36 +0000

    Add ClojureScript support with reader conditionals

--- project.clj
+++ project.clj
@@ -3,6 +3,19 @@
   :url "https://github.com/weavejester/meta-merge"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :aliases {"test-all" ["with-profile" "default:+1.6" "test"]}
-  :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}})
+  :plugins [[lein-cljsbuild "1.0.3"]
+            [lein-doo "0.1.6"]]
+  :aliases {"test-cljs" ["doo" "rhino" "test" "once"]
+            "test-all" ["do" ["test"] ["test-cljs"]]}
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :profiles {:provided
+             {:dependencies [[org.clojure/clojurescript "1.7.189"]]}
+             :test
+             {:dependencies [[org.mozilla/rhino "1.7.7"]]}}
+  :doo {:paths {:rhino "lein run -m org.mozilla.javascript.tools.shell.Main"}}
+  :cljsbuild {:builds
+              {:test {:source-paths ["src" "test"]
+                      :compiler {:output-to "target/test-runner.js"
+                                 :output-dir "target"
+                                 :main meta-merge.test-runner
+                                 :optimizations :simple}}}})
